485c73cb918f3457505cf2656bfa466cac0321b9
Olivier Pieters <me@olivierpieters.be>
2017-12-15 23:15:11 +0100

    Drastically simplified internal structure.

--- src/create_class_file.py
+++ src/create_class_file.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
             command += "\\NewDocumentCommand{%s}{%s}{%%\n" % (name, arguments)
             command += "  \\begingroup%\n"
             for j in range(n_args):
-                command += "  \\setkeys{rst@image%s}{%%\n" % chr(ord("A")+j+4*i)
+                command += "  \\setkeys{rst@image}{%%\n"
                 command += "    xoffset={0},\n"
                 command += "    yoffset={0},\n"
                 command += "    onlywidth={true},\n"
@@ -43,7 +43,6 @@ if __name__ == "__main__":
                 command += "  \\rzt@basicParseDefinitions{%s}{#%d}%%\n" % (chr(ord("A")+j+4*i), 2*j+2)
 
                 command += "  \\rzt@basicNodeDefinition%\n"
-                command += "    {%s}%%\n" % chr(ord("A") + j + 4*i)
                 command += "    {%s}%%\n" % frame["images"][j]["width"]
                 command += "    {%s}%%\n" % frame["images"][j]["height"]
                 command += "    {%s}%%\n" % frame["images"][j]["x_offset"]
