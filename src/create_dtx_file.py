94337f692f1ad3257b0994f2cbc7f7cff20687a5
Olivier Pieters <me@olivierpieters.be>
2017-12-22 08:24:45 +0100

    Start creating actual .dtx file. Only for documentation purposes now.

--- src/create_dtx_file.py
+++ src/create_dtx_file.py
@@ -2,52 +2,41 @@ import yaml,math
 
 if __name__ == "__main__":
     # load .cls file
-    source_file = ""
+    source_file = open("rozet_base.dtx").readlines()
 
     data = yaml.load(open("frame_definitions.yml"))
     data = [i for i in data if i["name"] != ""]
 
-    source_file += "\\documentclass{article}\n"
-    source_file += "\\usepackage{tikz}\n"
-    source_file += "\\usepackage{calc}\n"
-    source_file += "\\usepackage[landscape]{geometry}"
-    source_file += "\\usetikzlibrary{calc,positioning}\n"
-    source_file += "\\newlength\\rztmargin \\newlength\\rztbleed \\setlength\\rztmargin{1cm} \\setlength\\rztbleed{3mm} \\newlength\\rztinnersep \\setlength\\rztinnersep{0.3cm}"
-
-    source_file += "\\newlength\\rztpaperwidth \\newlength\\rztpaperheight\\setlength\\rztpaperwidth{0.2\\paperwidth}\\setlength\\rztpaperheight{0.2\\paperheight}\n"
-
-    source_file += "\\newlength\\onePageHorImageWidth \\newlength\onePageHorImageHeight \\newlength\\fourPageHorImageWidth \\newlength\\fourPageHorImageHeight \\newlength\\sixPageHorImageWidth \\newlength\\sixPageHorImageHeight \\newlength\\twoPageSpreadImageWidth \\newlength\\twoPageSpreadImageHeight\n"
-
-    source_file += "\\setlength\\onePageHorImageWidth{\\rztpaperwidth-2\\rztmargin-2\\rztbleed} \\setlength\\onePageHorImageHeight{2\\onePageHorImageWidth/3} \\setlength\\fourPageHorImageWidth{(\\onePageHorImageWidth-\\rztinnersep)/2} \\setlength\\fourPageHorImageHeight{(\\onePageHorImageHeight-\\rztinnersep)/2} \\setlength\\sixPageHorImageWidth{(\\onePageHorImageWidth-2\\rztinnersep)/3} \\setlength\\sixPageHorImageHeight{(\\onePageHorImageHeight-\\rztinnersep)/2} \\setlength\\twoPageSpreadImageWidth{\\rztpaperwidth-\\rztmargin} \\setlength\\twoPageSpreadImageHeight{\\rztpaperheight-2\\rztmargin}\n"
-
-    source_file += "\\begin{document}\n"
-
-
     for frame in data:
-      source_file += "\\clearpage"
-      source_file += "\\verb|%s|\n" % frame["name"]
+      source_file += "%% \subsection{\\texttt{%s}}\n%%\n" % frame["name"].replace("\\", "\\textbackslash ")
       
-      source_file += "\\begin{tikzpicture}\n"
+      source_file += "% \\begin{tikzpicture}[every node/.style={inner sep=0pt,outer sep=0pt}]\n"
 
       for j, i in enumerate(frame["images"]):
-        source_file += "\\node[\n"
-        source_file += "rectangle,\n"
-        source_file += "minimum width=%s,\n" % i["width"]
-        source_file += "minimum height=%s,\n" % i["height"]
-        source_file += "fill opacity=0.5,text opacity=1,fill=gray,\n"
-        source_file += "anchor=center,\n"
-        source_file += "] at ($(0,0)\n"
-        source_file += "+(%s,%s)$)\n" % (i["x_offset"], i["y_offset"])
-        source_file += "(rzt wrapper)\n"
-        source_file += "{%d};\n" % (j+1)
-        source_file += "\\node[above=0cm of rzt wrapper,draw,rectangle,text width=%s] {caption};\n" % i["width"]
+        source_file += "% \\node[\n"
+        source_file += "% rectangle,\n"
+        source_file += "%% minimum width=%s,\n" % i["width"]
+        source_file += "%% minimum height=%s,\n" % i["height"]
+        source_file += "% fill opacity=0.5,text opacity=1,fill=gray,\n"
+        source_file += "% anchor=center,\n"
+        source_file += "% ] at ($(0,0)\n"
+        source_file += "%% +(%s,%s)$)\n" % (i["x_offset"], i["y_offset"])
+        source_file += "% (rzt wrapper)\n"
+        source_file += "%% {%d};\n" % (j+1)
+        source_file += "%% \\node[above=0.025cm of rzt wrapper,draw,rectangle,text width=%s,inner sep=0,outer sep=0,fill=green,green] {};\n" % i["width"]
+        source_file += "% \\node[draw,rectangle,minimum width=\\rztpaperwidth+2\\rztbleed,minimum height=\\rztpaperheight+2\\rztbleed,anchor=center] at (0,0) {};\n"
 
-      source_file += "\\end{tikzpicture}\n"
+      source_file += "% \\end{tikzpicture}\n%\n"
+
+    source_file += "% \StopEventually{}"
+    source_file += "%"
+    source_file += "% \section{Implementation}"
+    source_file += "%"
 
     source_file += "\\end{document}\n"
     source_file += "\\endinput\n"
 
-    with open("rozet_dtx.tex", "w") as f:
+    with open("rozet.dtx", "w") as f:
         f.writelines(source_file)
 
 
